48fdc44a09bf7b5a3943e4a2913359e905008161
Jacody <Jacobscheer8@gmail.com>
2025-03-03 12:45:50 +0100

    merged all 4 GUIs

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ from PyQt5.QtGui import QFont
 # Importiere die drei Programme
 from tracker import MainApp  # Gesichtserkennung (Produktivitäts-Tracker)
 from todo_manager import TodoApp  # To-Do-Manager
-# Importiere die BarChartApp-Klasse aus csv_reader
-from csv_reader import DataManager, BarChartApp  # CSV-Visualisierung
+# Importiere die integrierte Visualisierung aus csv_reader
+from csv_reader import CsvVisualizerCombined  # Integrierte CSV-Visualisierung
 
 # Wrappen der MainApp als QWidget für Integration
 class ProductivityTracker(QWidget):
@@ -19,39 +19,16 @@ class ProductivityTracker(QWidget):
         self.layout.addWidget(self.main_app)
         self.setLayout(self.layout)
 
-# Wrappen der BarChartApp als QWidget für Integration
-class CsvVisualizer(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.layout = QVBoxLayout()
-        
-        # Daten-Manager initialisieren und Daten laden
-        data_manager = DataManager()
-        data_dict, total_actual_times, start_times = data_manager.load_all_data(verbose=False)
-        
-        # BarChartApp erstellen und dem Layout hinzufügen
-        self.chart_app = BarChartApp(data_dict, total_actual_times, start_times)
-        self.layout.addWidget(self.chart_app)
-        
-        self.setLayout(self.layout)
-
 # Haupt-App mit vertikalem Layout für alle drei Programme
 class CombinedApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Productivity-Tracker")
-        self.setGeometry(100, 100, 900, 1100)  # Etwas höher für mehr Platz
+        self.setGeometry(100, 100, 1000, 1100)  # Etwas höher und breiter für mehr Platz
 
         # Zentrales Widget mit vertikalem Layout
         central_widget = QWidget()
         main_layout = QVBoxLayout(central_widget)
-        
-        # Titel-Label
-        """title_label = QLabel("Produktivitäts-Suite")
-        title_font = QFont("Arial", 14, QFont.Bold)
-        title_label.setFont(title_font)
-        title_label.setAlignment(Qt.AlignCenter)
-        main_layout.addWidget(title_label)"""
         
         # Oberen Teil (ursprüngliches horizontales Split-Layout) erstellen
         top_splitter = QSplitter(Qt.Horizontal)
@@ -68,26 +45,15 @@ class CombinedApp(QMainWindow):
         separator = QLabel()
         separator.setStyleSheet("background-color: #cccccc; min-height: 2px; max-height: 2px;")
         
-        # CSV-Visualisierung im unteren Bereich hinzufügen
-        csv_widget = CsvVisualizer()
+        # Integrierte CSV-Visualisierung im unteren Bereich hinzufügen
+        csv_widget = CsvVisualizerCombined()
         
         # Elemente zum Layout hinzufügen
         main_layout.addWidget(top_splitter, 2)  # 2 = doppelte Gewichtung für den oberen Teil
         main_layout.addWidget(separator)
-        main_layout.addWidget(csv_widget, 3)   # 1 = einfache Gewichtung für die Visualisierung
-        
-        # Datumsanzeige hinzufügen
-        #date_label = QLabel(f"Datum: {self.get_current_date()}")
-        #date_label.setAlignment(Qt.AlignRight)
-        #main_layout.addWidget(date_label)
+        main_layout.addWidget(csv_widget, 3)   # 3 = dreifache Gewichtung für die Visualisierungen
         
         self.setCentralWidget(central_widget)
-    
-    '''
-    def get_current_date(self):
-        """Gibt das aktuelle Datum im deutschen Format zurück"""
-        from datetime import datetime
-        return datetime.now().strftime("%d.%m.%Y")'''
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
